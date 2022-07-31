fcc7f38bb8d59463a180b8bcfe81a3f72c7eb87d
Ichtyander <di_ignatov@mail.ru>
2022-07-31 21:43:22 +0300

    Task_2 completed

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -10,8 +10,24 @@ namespace Task_2
 {
     class Program
     {
+        static void GetCubeParams (int a, out double area, out double volume)
+        {
+            area = 6 * Math.Pow((double)a, 2);
+            volume = Math.Pow((double)a, 3);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Вычисление площади поверхности и объема куба");
+            Console.WriteLine();
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            double area, volume;
+            GetCubeParams(a, out area, out volume);
+
+            Console.WriteLine("Площадь куба - {0:.00}\nОбъём куба - {1:.00}", area, volume);
+            Console.ReadKey();
         }
     }
 }
